bc9784b93f7f228dd0e87e7ddd5d0e1c36c102c4
Tommaso Cellottini <tommasocellottini@gmail.com>
2026-01-18 12:14:42 +0100

    Add list selection, preview and quality slider.

--- Classes/FileEntry.cs
+++ Classes/FileEntry.cs
@@ -1,17 +1,20 @@
 namespace Velo.Classes
 {
 
-public class FileEntry {
-    public string FileId { get; set; } = "";
-    public string Name { get; set; } = "";
-    public long Size { get; set; } // Peso Originale
-    public bool Is420 { get; set; }
-    
-    // Campi per il post-ottimizzazione
-    public long? CompressedSize { get; set; }
-    public double? Savings { get; set; }
-    public string? Url { get; set; }
-    public byte[]? Data { get; set; }
-    public int Quality { get; set; } = 75; // Qualità specifica per questo file
-}
+    public class FileEntry
+    {
+        public string FileId { get; set; } = "";
+        public string Name { get; set; } = "";
+        public long Size { get; set; } // Peso Originale
+        public bool Is420 { get; set; }
+        public string? OriginalUrl { get; set; }
+
+
+        // Campi per il post-ottimizzazione
+        public long? CompressedSize { get; set; }
+        public double? Savings { get; set; }
+        public string? Url { get; set; }
+        public byte[]? Data { get; set; }
+        public int Quality { get; set; } = 75; // Qualità specifica per questo file
+    }
 }
